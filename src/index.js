4e5da3839bd78b04511ae69748542e778957e1be
Matt Casperson <mc.casperson@gmail.com>
2021-11-30 10:08:25 -0800

    add Ron Swanson, remove Bob Ross, WIP make buttons work
    Co-Authored-By: Isaac Overstreet <isaacoverstreet3@gmail.com>
    Co-authored-by: Jeff Terrell <jeffaterrell@gmail.com>
    Co-authored-by: Kate Kiatsiri <keidsiri@gmail.com>
    Co-authored-by: Chase Stokes <chasehstokes@gmail.com>
    Co-Authored-by: Marisa Edgar <marisaedgar1212@gmail.com>

--- src/index.js
+++ src/index.js
@@ -2,34 +2,37 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import BobRossService from "./js/exchangerate.js";
+import RonSwansonService from "./js/bob-ross-service.js";
 import DadService from './js/dad-joke-service';
 import ProgrammingService from "./js/programming-service";
+import SimpsonsService from './js/simpsons-service';
 
 
-function bobRoss(input) {
+function ronSwan(input) {
   if (input) {
-    BobRossService.getBobRossSearch(input)
-      .then(function(bobResponse) {
-        if (bobResponse instanceof Error) {
-          throw Error(`Bob Ross API error: ${bobResponse.message}`);
+    RonSwansonService.getRonSwanSearch(input)
+      .then(function(ronResponse) {
+        if (ronResponse instanceof Error) {
+          throw Error(`Ron Swanson API error: ${ronResponse.message}`);
         }
-        let randomBob = bobResponse.response.length - 1;
-        let bobChoice = Math.floor(Math.random() * randomBob); /*random number between 0 and randomBob */ 
-        const theJoke = bobResponse.response[bobChoice].quote;
+        let randomRon = ronResponse.length - 1;
+        let ronChoice = Math.floor(Math.random() * randomRon); /*random number between 0 and randomBob */ 
+        const theJoke = ronResponse[ronChoice];
         displayJoke(theJoke);
+        console.log(theJoke);
       })
       .catch(function(error) {
         displayErrors(error.message);
       });
   } else {
-    BobRossService.getBobRossRandom()
-      .then(function(bobResponse) {
-        if (bobResponse instanceof Error) {
-          throw Error(`Bob Ross API error: ${bobResponse.message}`);
+    RonSwansonService.getRonSwanRandom()
+      .then(function(ronResponse) {
+        if (ronResponse instanceof Error) {
+          throw Error(`Ron Swanson API error: ${ronResponse.message}`);
         }
-        const theJoke = bobResponse.response[0].quote;
+        const theJoke = ronResponse[0];
         displayJoke(theJoke);
+        console.log(theJoke);
       })
       .catch(function(error) {
         displayErrors(error.message);
@@ -40,24 +43,26 @@ function bobRoss(input) {
 function programming(input) {
   if (input) {
     ProgrammingService.getSearchJoke(input)
-    .then(function(programmingResponse) {
-      if(programmingResponse instanceof Error) {
-        throw Error (`Programming joke API error: ${programmingResponse.message}`);
-      }
-      const programmingJoke = programmingResponse.joke;
-      displayJoke(programmingJoke);
-    })
-    .catch(function(error) {
-      displayErrors(error.message);
-    });
+      .then(function(programmingResponse) {
+        if(programmingResponse instanceof Error) {
+          throw Error (`Programming joke API error: ${programmingResponse.message}`);
+        }
+        const programmingJoke = programmingResponse.joke;
+        displayJoke(programmingJoke);
+        console.log(programmingJoke);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
   } else {
     ProgrammingService.getRandomJoke()
       .then(function(programmingResponse) {
         if (programmingResponse instanceof Error) {
-          throw Error(`Prgramming joke API error: ${programmingResponse.message}`);
+          throw Error(`Programming joke API error: ${programmingResponse.message}`);
         }
-        const programmingJoke = programmingJoke.joke;
+        const programmingJoke = programmingResponse.joke;
         displayJoke(programmingJoke);
+        console.log(programmingJoke);
       })
       .catch(function(error) {
         displayErrors(error.message);
@@ -68,52 +73,95 @@ function programming(input) {
 function dadJoke(input) {
   if (input) {
     DadService.getSearchJoke(input)
-    .then(function(dadResponse) {
-      if(dadResponse instanceof Error) {
-        throw Error (`Dad Joke API error: ${dadResponse.message}`);
-      }
-      let randomDad = dadResponse.results.length - 1;
-      let dadChoice = Math.floor(Math.random() * randomDad);
-      const dadJoke = dadResponse.results[dadChoice].joke;
-      displayJoke(dadJoke);
-    })
-    .catch(function(error) {
-      displayErrors(error.message);
-    });
+      .then(function(dadResponse) {
+        if(dadResponse instanceof Error) {
+          throw Error (`Dad Joke API error: ${dadResponse.message}`);
+        }
+        let randomDad = dadResponse.results.length - 1;
+        let dadChoice = Math.floor(Math.random() * randomDad);
+        const dadJoke = dadResponse.results[dadChoice].joke;
+        displayJoke(dadJoke);
+        console.log(dadJoke);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
   } else {
     DadService.getRandomJoke()
-    .then(function(dadResponse) {
-      if (dadResponse instanceof Error) {
-        throw Error(`Dad Joke API error: ${dadResponse.message}`);
-      }
-      const dadJoke = dadResponse.results.joke;
-      displayJoke(dadJoke);
-    })
-    .catch(function(error) {
-      displayErrors(error.message);
-    });
+      .then(function(dadResponse) {
+        if (dadResponse instanceof Error) {
+          throw Error(`Dad Joke API error: ${dadResponse.message}`);
+        }
+        const dadJoke = dadResponse.joke;
+        displayJoke(dadJoke);
+        console.log(dadJoke);
+      })
+      .catch(function(error) {
+        displayErrors(error.message);
+      });
   }
 }
 
-function simpsons () {
+function simpsons() {
   SimpsonsService.getSimpsonsRandom()
-  .then(function(simpsonsResponse) {
-    if (simpsonsResponse instanceof Error) {
-      throw Error (`Simpson quote API erre : ${simpsonsResponse.message}`);
-    }
-    const simpsonsJoke = simpsonsResponse.quote;
-    const simpsonsImage = simpsonsResponse.image;
-    displayJoke(simpsonsJoke);
-    displayImage(simpsonsImage);
-  })
-  .catch(function(error) {
-    displayErrors(error.message);
-  });
+    .then(function(simpsonsResponse) {
+      if (simpsonsResponse instanceof Error) {
+        throw Error (`Simpson quote API error: ${simpsonsResponse.message}`);
+      }
+      const simpsonsJoke = simpsonsResponse[0].quote;
+      const simpsonsImage = simpsonsResponse.image;
+      displayJoke(simpsonsJoke);
+      displayImage(simpsonsImage);
+      console.log(simpsonsJoke);
+    })
+    .catch(function(error) {
+      displayErrors(error.message);
+    });
 }
 
+function displayJoke() {
+
+}
+
+function displayErrors() {
+
+}
+
+function displayImage() {
+
+}
+
+function clearAllFields() {
+  $('input').val('');
+}
 
 $(document).ready(function() {
-  $('#jokeType').click(function() {
-    
+  $('form').submit(function(event) {
+    event.preventDefault();
+    clearAllFields();
+  });
+  $('#probtn').click(function() {
+    let progInput = $('#prog').val();
+    programming(progInput);
+  });
+  $('#probtn2').click(function() {
+    programming('');
+  });
+  $('#dadbtn').click(function() {
+    let dadInput = $('#dad').val();
+    dadJoke(dadInput);
+  });
+  $('#dadbtn2').click(function() {
+    dadJoke('');
+  });
+  $('#ronbtn').click(function() {
+    let ronInput = $('#rons').val();
+    ronSwan(ronInput);
+  });
+  $('#ronbtn2').click(function() {
+    ronSwan('');
+  });
+  $('#simpbtn').click(function() {
+    simpsons('');
   });
 });    
--- src/js/bob-ross-service.js
+++ src/js/bob-ross-service.js
@@ -1,27 +1,27 @@
-export default class BobRossService {
-  static getBobRossRandom() {
-    return fetch (`https://api.bobross.dev/api`)
-    .then (function(response) {
-      if (!response.ok) {
-        throw Error (response.statusText);
-      }
-      return response.json();  
-    })
-    .catch (function(error) {
-      return Error(error);
-    }); 
+export default class RonSwansonService {
+  static getRonSwanRandom() {
+    return fetch (`https://ron-swanson-quotes.herokuapp.com/v2/quotes/`)
+      .then (function(response) {
+        if (!response.ok) {
+          throw Error (response.statusText);
+        }
+        return response.json();  
+      })
+      .catch (function(error) {
+        return Error(error);
+      }); 
   }
 
-  static getBobRossSearch(input) {
-    return fetch (`https://api.bobross.dev/api/search/${input}`)
-    .then (function(response) {
-      if (!response.ok) {
-        throw Error (response.statusText);
-      }
-      return response.json();  
-    })
-    .catch (function(error) {
-      return Error(error);
-    }); 
+  static getRonSwanSearch(input) {
+    return fetch (`https://ron-swanson-quotes.herokuapp.com/v2/quotes/search/${input}`)
+      .then (function(response) {
+        if (!response.ok) {
+          throw Error (response.statusText);
+        }
+        return response.json();  
+      })
+      .catch (function(error) {
+        return Error(error);
+      }); 
   }
 }
